cfd44d93eae4e87ef38941ef7157e44afbd619c8
E. Collins <eoghancollins0@gmail.com>
2024-08-29 15:31:01 +0100

    no

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
     basePath: "/2048-in-react",
     output: "export",  // <=== enables static exports
-    reactStrictMode: true,  
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+      },
+      assetPrefix: process.env.NODE_ENV === 'production' ? '/<REPO_NAME>' : '',
+      basePath: process.env.NODE_ENV === 'production' ? '/<REPO_NAME>' : '',
 };
 
 export default nextConfig;
