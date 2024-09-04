a17f38b8d7f2602130defa39fff9c53ae16b8599
E. Collins <eoghancollins0@gmail.com>
2024-09-04 13:15:06 +0100

    Throwing stuff at a wall to see what sticks

--- src/app/creative/blog/page.tsx
+++ src/app/creative/blog/page.tsx
@@ -1,28 +1,18 @@
-// // app/creative/blog/page.tsx
-// import React, { Suspense } from 'react';
-// import dynamic from 'next/dynamic';
+// app/creative/blog/page.tsx
+"use client"
+import React, { Suspense } from 'react';
+import dynamic from 'next/dynamic';
 
-// const BlogPageContent = dynamic(() => import('./BlogPageContent'), { ssr: false });
+const BlogPageContent = dynamic(() => import('./BlogPageContent'), { ssr: false });
 
-// const LoadingFallback = () => <div>Loading...</div>;
+const LoadingFallback = () => <div>Loading...</div>;
 
-// const BlogPage: React.FC = () => {
-//   return (
-//     <Suspense fallback={<LoadingFallback />}>
-//       <BlogPageContent />
-//     </Suspense>
-//   );
-// };
-
-// export default BlogPage;
-
-import { Suspense } from 'react';
-import BlogPage from './BlogPageContent';
-
-export default function Page() {
+const BlogPage: React.FC = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <BlogPage />
+    <Suspense fallback={<LoadingFallback />}>
+      <BlogPageContent />
     </Suspense>
   );
-}
+};
+
+export default BlogPage;
